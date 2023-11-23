07c4b0dc43c1c851842e4e6d9f1bf62dda1c9c02
violettomania <danielpolya@protonmail.com>
2023-11-23 21:00:17 +0100

    setup: display data from data source

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,17 @@
+import logo from '../images/logo.svg';
+import phoneImg from '../images/phone.svg';
+import subLinks from '../data';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 export default function App() {
   return (
     <>
       <nav className='nav'>
         <div className='nav-center'>
           <div className='nav-header'>
-            <img src='./images/logo.svg' className='nav-logo' alt='' />
+            <img src={logo} className='nav-logo' alt='' />
             <button className='btn toggle-btn'>
-              <svg
-                stroke='currentColor'
-                fill='currentColor'
-                stroke-width='0'
-                viewBox='0 0 448 512'
-                height='1em'
-                width='1em'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path d='M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z'></path>
-              </svg>
+              <FaBars />
             </button>
           </div>
           <ul className='nav-links'>
@@ -36,159 +31,29 @@ export default function App() {
       <div className='sidebar-wrapper'>
         <aside className='sidebar'>
           <button className='close-btn'>
-            <svg
-              stroke='currentColor'
-              fill='currentColor'
-              stroke-width='0'
-              viewBox='0 0 352 512'
-              height='1em'
-              width='1em'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path d='M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z'></path>
-            </svg>
+            <FaTimes />
           </button>
           <div className='sidebar-links'>
             <article>
-              <h4>products</h4>
-              <div className='sidebar-sublinks'>
-                <a href='/products'>
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    stroke-width='0'
-                    viewBox='0 0 576 512'
-                    height='1em'
-                    width='1em'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M0 432c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V256H0v176zm192-68c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H204c-6.6 0-12-5.4-12-12v-40zm-128 0c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM576 80v48H0V80c0-26.5 21.5-48 48-48h480c26.5 0 48 21.5 48 48z'></path>
-                  </svg>
-                  payment
-                </a>
-                <a href='/products'>
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    stroke-width='0'
-                    viewBox='0 0 576 512'
-                    height='1em'
-                    width='1em'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M0 432c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V256H0v176zm192-68c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H204c-6.6 0-12-5.4-12-12v-40zm-128 0c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM576 80v48H0V80c0-26.5 21.5-48 48-48h480c26.5 0 48 21.5 48 48z'></path>
-                  </svg>
-                  terminal
-                </a>
-                <a href='/products'>
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    stroke-width='0'
-                    viewBox='0 0 576 512'
-                    height='1em'
-                    width='1em'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M0 432c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V256H0v176zm192-68c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H204c-6.6 0-12-5.4-12-12v-40zm-128 0c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM576 80v48H0V80c0-26.5 21.5-48 48-48h480c26.5 0 48 21.5 48 48z'></path>
-                  </svg>
-                  connect
-                </a>
-              </div>
-            </article>
-            <article>
-              <h4>developers</h4>
-              <div className='sidebar-sublinks'>
-                <a href='/products'>
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    stroke-width='0'
-                    viewBox='0 0 448 512'
-                    height='1em'
-                    width='1em'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M448 360V24c0-13.3-10.7-24-24-24H96C43 0 0 43 0 96v320c0 53 43 96 96 96h328c13.3 0 24-10.7 24-24v-16c0-7.5-3.5-14.3-8.9-18.7-4.2-15.4-4.2-59.3 0-74.7 5.4-4.3 8.9-11.1 8.9-18.6zM128 134c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm0 64c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm253.4 250H96c-17.7 0-32-14.3-32-32 0-17.6 14.4-32 32-32h285.4c-1.9 17.1-1.9 46.9 0 64z'></path>
-                  </svg>
-                  plugins
-                </a>
-                <a href='/products'>
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    stroke-width='0'
-                    viewBox='0 0 448 512'
-                    height='1em'
-                    width='1em'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M448 360V24c0-13.3-10.7-24-24-24H96C43 0 0 43 0 96v320c0 53 43 96 96 96h328c13.3 0 24-10.7 24-24v-16c0-7.5-3.5-14.3-8.9-18.7-4.2-15.4-4.2-59.3 0-74.7 5.4-4.3 8.9-11.1 8.9-18.6zM128 134c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm0 64c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm253.4 250H96c-17.7 0-32-14.3-32-32 0-17.6 14.4-32 32-32h285.4c-1.9 17.1-1.9 46.9 0 64z'></path>
-                  </svg>
-                  libraries
-                </a>
-                <a href='/products'>
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    stroke-width='0'
-                    viewBox='0 0 448 512'
-                    height='1em'
-                    width='1em'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M448 360V24c0-13.3-10.7-24-24-24H96C43 0 0 43 0 96v320c0 53 43 96 96 96h328c13.3 0 24-10.7 24-24v-16c0-7.5-3.5-14.3-8.9-18.7-4.2-15.4-4.2-59.3 0-74.7 5.4-4.3 8.9-11.1 8.9-18.6zM128 134c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm0 64c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm253.4 250H96c-17.7 0-32-14.3-32-32 0-17.6 14.4-32 32-32h285.4c-1.9 17.1-1.9 46.9 0 64z'></path>
-                  </svg>
-                  help
-                </a>
-                <a href='/products'>
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    stroke-width='0'
-                    viewBox='0 0 448 512'
-                    height='1em'
-                    width='1em'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M448 360V24c0-13.3-10.7-24-24-24H96C43 0 0 43 0 96v320c0 53 43 96 96 96h328c13.3 0 24-10.7 24-24v-16c0-7.5-3.5-14.3-8.9-18.7-4.2-15.4-4.2-59.3 0-74.7 5.4-4.3 8.9-11.1 8.9-18.6zM128 134c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm0 64c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm253.4 250H96c-17.7 0-32-14.3-32-32 0-17.6 14.4-32 32-32h285.4c-1.9 17.1-1.9 46.9 0 64z'></path>
-                  </svg>
-                  billing
-                </a>
-              </div>
-            </article>
-            <article>
-              <h4>company</h4>
-              <div className='sidebar-sublinks'>
-                <a href='/products'>
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    stroke-width='0'
-                    viewBox='0 0 512 512'
-                    height='1em'
-                    width='1em'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M320 336c0 8.84-7.16 16-16 16h-96c-8.84 0-16-7.16-16-16v-48H0v144c0 25.6 22.4 48 48 48h416c25.6 0 48-22.4 48-48V288H320v48zm144-208h-80V80c0-25.6-22.4-48-48-48H176c-25.6 0-48 22.4-48 48v48H48c-25.6 0-48 22.4-48 48v80h512v-80c0-25.6-22.4-48-48-48zm-144 0H192V96h128v32z'></path>
-                  </svg>
-                  about
-                </a>
-                <a href='/products'>
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    stroke-width='0'
-                    viewBox='0 0 512 512'
-                    height='1em'
-                    width='1em'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M320 336c0 8.84-7.16 16-16 16h-96c-8.84 0-16-7.16-16-16v-48H0v144c0 25.6 22.4 48 48 48h416c25.6 0 48-22.4 48-48V288H320v48zm144-208h-80V80c0-25.6-22.4-48-48-48H176c-25.6 0-48 22.4-48 48v48H48c-25.6 0-48 22.4-48 48v80h512v-80c0-25.6-22.4-48-48-48zm-144 0H192V96h128v32z'></path>
-                  </svg>
-                  customers
-                </a>
-              </div>
+              {subLinks.map((item, index) => {
+                const { links, page } = item;
+                return (
+                  <div key={index}>
+                    <h4>{page}</h4>
+                    <div className='sidebar-sublinks'>
+                      {links.map((link, index) => {
+                        const { url, icon, label } = link;
+                        return (
+                          <a href={url} key={index}>
+                            {icon}
+                            {label}
+                          </a>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })}
             </article>
           </div>
         </aside>
@@ -208,57 +73,31 @@ export default function App() {
             <button className='btn'>Start now</button>
           </article>
           <article className='hero-images'>
-            <img src='./images/phone.svg' className='phone-img' alt='phone' />
+            <img src={phoneImg} className='phone-img' alt='phone' />
           </article>
         </div>
       </section>
       <aside className='submenu' style={{ left: '337.539px', top: '61.5px' }}>
         <section>
-          <h4>products</h4>
-          <div className='submenu-center col-3'>
-            <a href='/products'>
-              <svg
-                stroke='currentColor'
-                fill='currentColor'
-                stroke-width='0'
-                viewBox='0 0 576 512'
-                height='1em'
-                width='1em'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path d='M0 432c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V256H0v176zm192-68c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H204c-6.6 0-12-5.4-12-12v-40zm-128 0c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM576 80v48H0V80c0-26.5 21.5-48 48-48h480c26.5 0 48 21.5 48 48z'></path>
-              </svg>
-              payment
-            </a>
-            <a href='/products'>
-              <svg
-                stroke='currentColor'
-                fill='currentColor'
-                stroke-width='0'
-                viewBox='0 0 576 512'
-                height='1em'
-                width='1em'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path d='M0 432c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V256H0v176zm192-68c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H204c-6.6 0-12-5.4-12-12v-40zm-128 0c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM576 80v48H0V80c0-26.5 21.5-48 48-48h480c26.5 0 48 21.5 48 48z'></path>
-              </svg>
-              terminal
-            </a>
-            <a href='/products'>
-              <svg
-                stroke='currentColor'
-                fill='currentColor'
-                stroke-width='0'
-                viewBox='0 0 576 512'
-                height='1em'
-                width='1em'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path d='M0 432c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V256H0v176zm192-68c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H204c-6.6 0-12-5.4-12-12v-40zm-128 0c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM576 80v48H0V80c0-26.5 21.5-48 48-48h480c26.5 0 48 21.5 48 48z'></path>
-              </svg>
-              connect
-            </a>
-          </div>
+          {subLinks.map((item, index) => {
+            const { links, page } = item;
+            return (
+              <div key={index}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {links.map((link, index) => {
+                    const { url, icon, label } = link;
+                    return (
+                      <a href={url} key={index}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
         </section>
       </aside>
     </>
