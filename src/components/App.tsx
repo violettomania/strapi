384025685145e359f6c6e1cc0e749c2124440c80
violettomania <danielpolya@protonmail.com>
2023-11-25 10:54:23 +0100

    refactor: add Hero component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
-import phoneImg from '../images/phone.svg';
 import subLinks from '../data';
+import { Hero } from './Hero';
 import { Navbar } from './Navbar';
 import { Sidebar } from './Sidebar';
 
@@ -8,25 +8,7 @@ export default function App() {
     <>
       <Navbar />
       <Sidebar />
-      <section className='hero'>
-        <div className='hero-center'>
-          <article className='hero-info'>
-            <h1>
-              Payments infrastructure <br />
-              for the internet
-            </h1>
-            <p>
-              Millions of companies of all sizes—from startups to Fortune
-              500s—use Stripe’s software and APIs to accept payments, send
-              payouts, and manage their businesses online.
-            </p>
-            <button className='btn'>Start now</button>
-          </article>
-          <article className='hero-images'>
-            <img src={phoneImg} className='phone-img' alt='phone' />
-          </article>
-        </div>
-      </section>
+      <Hero />
       <aside className='submenu' style={{ left: '337.539px', top: '61.5px' }}>
         <section>
           {subLinks.map((item, index) => {
