39e8c1e64f184c539cb23eb47c3031237d1b329e
violettomania <danielpolya@protonmail.com>
2023-11-25 14:49:58 +0100

    refactor: move submenu item show condition to function

--- src/components/Submenu.tsx
+++ src/components/Submenu.tsx
@@ -2,6 +2,10 @@ import { useAppSelector } from '../hooks/hooks';
 import subLinks from '../data';
 import SubmenuItem from './SubmenuItem';
 
+const isSubmenuShown = (selectedSubmenuIndex: number) => {
+  return selectedSubmenuIndex >= 0;
+};
+
 export default function Submenu() {
   const selectedSubmenuIndex = useAppSelector(
     (state) => state.submenu.selectedIndex
@@ -9,14 +13,14 @@ export default function Submenu() {
 
   return (
     <aside
-      className={`submenu ${selectedSubmenuIndex >= 0 && 'show'}`}
+      className={`submenu ${isSubmenuShown(selectedSubmenuIndex) && 'show'}`}
       style={{
         left: 337.539 + selectedSubmenuIndex * 160,
         top: 61.5,
       }}
     >
       <section>
-        {selectedSubmenuIndex >= 0 && (
+        {isSubmenuShown(selectedSubmenuIndex) && (
           <SubmenuItem
             key={subLinks[selectedSubmenuIndex].page}
             {...subLinks[selectedSubmenuIndex]}
